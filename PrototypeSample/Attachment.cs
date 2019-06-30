dac04eae464438257de4c5e2f7b8abc5d78ae254
liupenghao <haochen214@163.com>
2019-06-30 20:01:06 +0800

    附件类

---
+++ PrototypeSample/Attachment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeSample
+{
+    /// <summary>
+    /// 附件类
+    /// </summary>
+    class Attachment
+    {
+        public string Name { get; set; }
+
+        public void DownLoad()
+        {
+            Console.WriteLine("下载附件，文件名为{0}。", Name);
+        }
+
+    }
+}
---
+++ PrototypeSample/WeeklyLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeSample
+{
+    /// <summary>
+    /// 周报类，充当原型角色
+    /// </summary>
+    class WeeklyLog
+    {
+        public Attachment attachment { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Content { get; set; }
+
+        // 使用MemberwiseClone()方法实现浅克隆
+        public WeeklyLog Clone()
+        {
+            return this.MemberwiseClone() as WeeklyLog;     //类型转换
+        }
+    }
+}
